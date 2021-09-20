54ae29e3ac8f9b8d0263eda0b52a203f3eafd781
Isabelly Dias <isabellydias29@gmail.com>
2021-09-20 20:32:52 -0300

    projeto inicial

---
+++ app/src/main/java/br/com/dev4u/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.dev4u
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
