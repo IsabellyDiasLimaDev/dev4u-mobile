f2172acb618a9202c0a57fca1ccd4ca1cf92b58c
Isabelly Dias <isabellydias29@gmail.com>
2021-09-21 18:20:33 -0300

    criacao da segunda tela

--- app/src/main/java/br/com/dev4u/MainActivity.kt
+++ app/src/main/java/br/com/dev4u/MainActivity.kt
@@ -1,11 +1,33 @@
 package br.com.dev4u
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_main)
+        setContentView(R.layout.activity_main)
+
+        var btLogin = findViewById(R.id.btLogin) as Button
+        var etUsername = findViewById(R.id.etUsername) as EditText
+        var etPassword = findViewById(R.id.etPassword) as EditText
+
+        btLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if (username == "aluno" && password == "impacta"){
+                Toast.makeText(this, "usuario e senha corretos", Toast.LENGTH_LONG).show()
+                var itent = Intent (this, SegundaActivity::class.java)
+                startActivity(itent)
+            }
+            else{
+                Toast.makeText(this, "usuario e senha incorretos", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/br/com/dev4u/SegundaActivity.kt
@@ -0,0 +1,14 @@
+package br.com.dev4u
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class SegundaActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.segunda_activity)
+
+    }
+}
