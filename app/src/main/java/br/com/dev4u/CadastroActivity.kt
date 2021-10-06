4d38055361dfc2fc43b3207aaeac82e924c85d0c
Isabelly Dias <isabellydias29@gmail.com>
2021-10-05 21:55:39 -0300

    Ajuste na toolbar e criação da tela de CadastroActivity.kt

--- app/src/main/java/br/com/dev4u/CadastroActivity.kt
+++ app/src/main/java/br/com/dev4u/CadastroActivity.kt
@@ -2,47 +2,27 @@ package br.com.dev4u
 
 import android.content.Context
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.SearchView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.toolbar.*
 
-
-class SegundaActivity: AppCompatActivity() {
+class CadastroActivity : AppCompatActivity() {
 
     private val context: Context get() = this
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.segunda_activity)
+        setContentView(R.layout.activity_cadastro)
 
         setSupportActionBar(toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
         menuInflater.inflate(R.menu.menu_main, menu)
-
-        (menu?.findItem(R.id.action_buscar)?.actionView as SearchView?)?.setOnQueryTextListener(
-            object : SearchView.OnQueryTextListener {
-
-
-                override fun onQueryTextChange(newText: String): Boolean {
-
-                    return false
-                }
-
-                override fun onQueryTextSubmit(query: String): Boolean {
-
-                    return false
-                }
-
-            })
         return true
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
